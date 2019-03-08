2c0abbee5d14724a689901dc2e8de9914b8d73ed
Sebastian Vaca <svaca@broadinstitute.org>
2019-03-08 11:35:41 -0500

    Changed update_AvG_RPackage and updated ExtTool v0.0.1.0

--- skyline_external_tool/AvG_skylineexternaltool/Update_AvG_RPackage.r
+++ skyline_external_tool/AvG_skylineexternaltool/Update_AvG_RPackage.r
@@ -4,8 +4,8 @@ Latest_version = "0.0.1.0"
 Avg_RPackage_path<-file.path(args[5],"AvG_R_Package", paste0("AvantGardeDIA_", Latest_version,".tar.gz"))
 
 ## Install AvantGarde Package if it is not already installed
-a<-installed.packages()
-packages<-a[,1]
-if (!is.element("AvantGardeDIA",packages) || packageVersion("AvantGardeDIA") < Latest_version ){
+instaled_pckgs<-installed.packages()
+instaled_pckgs<-instaled_pckgs[,1]
+if (!is.element("AvantGardeDIA",instaled_pckgs) || packageVersion("AvantGardeDIA") < Latest_version ){
   install.packages(Avg_RPackage_path,repos = NULL)
 }
